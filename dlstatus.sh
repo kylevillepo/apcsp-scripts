4247969ff625f7a4d47346b966a9d76ce7f1ded6
Kyle Villeponteau <villeponteauk1553@my.sduhsd.net>
2020-12-18 21:44:41 +0000

    modified file

--- dlstatus.sh
+++ dlstatus.sh
@@ -7,7 +7,7 @@ DROPLETS="dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11"
 
 for droplet in $DROPLETS ; do
    echo $droplet
-   ping -c 1 $droplet
+   ping $droplet > /dev/null
    if [ $? -eq 0 ] ; then
       echo ":alive"
    else
